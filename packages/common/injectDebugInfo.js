166d1c1989b77ca56876aaa90a9e56061507370c
wanganqing <249736701@qq.com>
2025-06-16 11:48:27 +0800

    init 2

--- packages/common/injectDebugInfo.js
+++ packages/common/injectDebugInfo.js
@@ -9,6 +9,7 @@ export function injectDebugInfoToVueCode(code, filename, tags = ['el-button'], o
         const tag = match[1];
         const insert = ` data-debug-tag=\"${tag}\" data-debug-filename=\"${filename}\" data-debug-line=\"${index + 1 + addLineNumber}\" v-line-copy `;
         const insertPos = line.indexOf(tag) + tag.length;
+
         return line.slice(0, insertPos) + insert + line.slice(insertPos);
     }).join('\n');
 }
